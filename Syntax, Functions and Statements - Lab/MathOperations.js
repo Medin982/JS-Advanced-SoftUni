b750e344c6ac3b71496da95b11be020889125253
Medin Tarakchiev <warss982@gmail.com>
2022-05-24 15:30:58 +0300

    Basic JS Exercises

---
+++ Syntax, Functions and Statements - Lab/MathOperations.js
@@ -0,0 +1,29 @@
+function mathOPerations(num1, num2, operator) {
+    let result;
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        case '%':
+            result = num1 % num2;
+            break;
+        case '**':
+            result = num1 ** num2;
+            break
+    }
+
+    console.log(result);
+}
+
+mathOPerations(5, 6, '+');
+
+mathOPerations(3, 5.5, '*');
---
+++ Syntax, Functions and Statements - Lab/echo.js
@@ -0,0 +1,8 @@
+function echo(a) {
+    console.log(a.length);
+    console.log(a);
+}
+
+echo('Hello, JavaScript!');
+
+echo('strings are easy');
