e9a061080efed09288a6389ac6d8e68529f40576
Medin Tarakchiev <medin123@abv.bg>
2022-10-07 07:42:38 +0300

    DOM Manipulation and Events exercises

--- DOM Manipulations and Events/execise/08. Sudomu/solution.js
+++ DOM Manipulations and Events/execise/08. Sudomu/solution.js
@@ -2,19 +2,39 @@ function solve() {
     let buttons = document.querySelectorAll('button');
     buttons[0].addEventListener('click', checkSudomu);
     buttons[1].addEventListener('click', clearSudamu);
-    let table = document.querySelectorAll('tbody tr');
+    let table = Array.from(document.querySelectorAll('table'));
+    let inputs = document.querySelectorAll('input');
 
     function checkSudomu() {
-        let sum = 0;
-        for (let row of table) {
-            let ele = row.querySelectorAll('td');
-            for (let e of ele) {
-            
+        let matrix = [
+            [inputs[0].value, inputs[1].value, inputs[2].value],
+            [inputs[3].value, inputs[4].value, inputs[5].value],
+            [inputs[6].value, inputs[7].value, inputs[8].value]
+        ];
+        isSudomu = true;
+        for (let i = 1; i < matrix.length; i++) {
+            let row = matrix[i];
+            let col = matrix.map(row => row[i]);
+            if (col.length != [...new Set(col)].length || row.length != [...new Set(row)].length) {
+                isSudomu = false;
+                break;
             }
+        }
+        if (isSudomu) {
+            table.style.border = '2px solid green';
+            checkPar.style.color = 'green';
+            checkPar.textContent = 'You solve it! Congratulations!';
+        } else {
+            table.style.border = '2px solid red';
+            checkPar.style.color = 'red';
+            checkPar.textContent = 'NOP! You are not done yet...';
         }
     }
 
     function clearSudamu() {
-
+        [...inputs].forEach(input => (input.value = ''));
+        table.style.border = 'none';
+        checkPar.textContent = '';
     }
 }
+
