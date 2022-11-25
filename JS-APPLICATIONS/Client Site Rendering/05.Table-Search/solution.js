6c49d690d292f4a50767a783999488e09c85946e
Medin Tarakchiev <medin123@abv.bg>
2022-11-25 12:09:28 +0200

    Client Side Rendering exercise

--- JS-APPLICATIONS/Client Site Rendering/05.Table-Search/solution.js
+++ JS-APPLICATIONS/Client Site Rendering/05.Table-Search/solution.js
@@ -1,8 +1,41 @@
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+
+
+solve();
+
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+   async function onClick() {
+      const input = document.getElementById('searchField');
+      const text = input.value.toLowerCase();
+      const response = await fetch('http://localhost:3030/jsonstore/advanced/table');
+      const data = await response.json();
+      const dataArr = Object.values(data);
 
+      render(createTable(dataArr, text ? text : undefined), document.querySelector('tbody'));
+      input.value = '';
+   }
+
+   function createTable(dataArr, match) {
+      return html`
+      ${dataArr.map(d => html`
+      <tr class=${matchText(d, match) ? 'select' : ''}>
+         <td>${d.firstName + ' ' + d.lastName}</td>
+         <td> ${d.email} </td>
+         <td>${d.course}</td>
+      </tr>`)}`;
+   }
+
+   function matchText(data, match) {
+      if (data.firstName.toLowerCase().startsWith(match) ||
+         data.lastName.toLowerCase().startsWith(match) ||
+         data.email.toLowerCase().startsWith(match) ||
+         data.course.toLowerCase().startsWith(match)) {
+
+         return true;
+      }
+
+      return false;
    }
 }
--- JS-APPLICATIONS/Client Site Rendering/06.Book-Library/app.js
+++ JS-APPLICATIONS/Client Site Rendering/06.Book-Library/app.js
@@ -1,1 +1,50 @@
-console.log('My requests...')
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+
+const body = document.getElementsByTagName('body')[0];
+
+update();
+
+function loadPage() { 
+    return html`
+    <button id="loadBooks" @click=${showBooks}>LOAD ALL BOOKS</button>
+    <table>
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+    
+        </tbody>
+    </table>
+    
+    <form id="add-form">
+        <h3>Add book</h3>
+        <label>TITLE</label>
+        <input type="text" name="title" placeholder="Title...">
+        <label>AUTHOR</label>
+        <input type="text" name="author" placeholder="Author...">
+        <input type="submit" value="Submit">
+    </form>`;
+}
+
+function update() {
+    render(loadPage(), body);
+}
+
+async function showBooks() {
+    const data = await getBooks();
+    
+}
+
+function loadBooks(books) {
+    return html`
+    `
+}
+
+async function getBooks() {
+    const response = await fetch(' http://localhost:3030/jsonstore/collections/books');
+    const data = await response.json();
+}
